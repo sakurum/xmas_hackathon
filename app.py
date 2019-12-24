ca833631b9865cbf736146e292cd1c2f021b8e31
Hide1204 <hirahide1204@gmail.com>
2019-12-25 00:59:16 +0900

    実行できるように変更

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    # webサーバー立ち上げ
+app.run(port=8000,debug=True)
